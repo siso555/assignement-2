0160b2e600eb89fc01005e2fe6ef86b60186956a
si <g.sisauri1987@gmail.com>
2022-06-30 20:52:06 +0400

    first comit

--- src/Assignement2.java
+++ src/Assignement2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class davaleba2 {
+public class Assignement2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("შემოიყვანეთ რიცხვი; ");
